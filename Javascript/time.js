c960a1bd6e2f9075423e07baedfc8ae58981b942
tomsenior20 <tomsenior20@gmail.com>
2023-07-16 11:15:06 +0100

    Time functionality handled and mobile sorted

--- Javascript/time.js
+++ Javascript/time.js
@@ -1,22 +1,43 @@
 var timeFunctionRan = false;
 
-document.getElementById("timeButton").addEventListener("click", (e) => {
-    e.preventDefault();
-    if (timeFunctionRan === false) {
-        getTime();
-    } else {
-        return;
-    }
-});
+window.onload = () => {
+    document.getElementById("timeButton").addEventListener("click", handleClick);
+};
 
-function getTime() {
-    var time = "";
-    todaysDate = new Date().toLocaleDateString();
-    localBSTTime = new Date().toLocaleTimeString("EN-GB");
-    returnedString = todaysDate + " " + localBSTTime;
-    time += `<div class="col-6 justify-content-start align-items-center d-flex"><p class="localBSTTime fs-4 my-0 mx-3"> ${returnedString} </p></div>`;
-    document.getElementById("secondSection").insertAdjacentHTML("beforeend", time);
-    document.getElementById("timeContainer").classList.remove("justify-content-center");
-    document.getElementById("timeContainer").classList.add("justify-content-end");
+const handleClick = (e) => {
+    e.preventDefault();
+    timeFunctionRan === false ? getTime() : hideTime();
+};
+
+const getTime = () => {
+    const todaysDate = new Date().toLocaleDateString();
+    const localBSTTime = new Date().toLocaleTimeString("en-GB");
+    const returnedString = `${todaysDate} ${localBSTTime}`;
+
+    const timeString = `<div id="timeString" class="col-6 justify-content-start align-items-center d-flex"><p class="localBSTTime fs-4 my-0 mx-3">${returnedString}</p></div>`;
+    document.getElementById("secondSection").insertAdjacentHTML("beforeend", timeString);
+
+    const timeContainer = document.getElementById("timeContainer");
+    timeContainer.classList.remove("justify-content-center");
+    timeContainer.classList.add("justify-content-end");
+
     timeFunctionRan = true;
+
+    const timeButton = document.getElementById("timeButton");
+    timeButton.innerHTML = "Close Local Time";
+};
+
+
+
+const hideTime = () => {
+    const timeSection = document.getElementById("timeString");
+    timeSection.remove();
+
+    const timeContainer = document.getElementById('timeContainer');
+    timeContainer.classList.add("justify-content-center");
+
+    timeFunctionRan = false;
+
+    const timeButton = document.getElementById("timeButton");
+    timeButton.innerHTML = "Click for Local Time";
 }
