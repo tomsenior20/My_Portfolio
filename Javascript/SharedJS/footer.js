2910d204e1a00b8584a48e195539a02431fe1e09
tomsenior20 <tomsenior20@gmail.com>
2023-07-17 17:54:22 +0100

    Footer Made W-100

---
+++ Javascript/SharedJS/footer.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function () {
+    // Code to be executed when the page has finished loading
+    console.log("Page loaded!");
+    getOperatingYear();
+});
+
+const getOperatingYear = () => {
+    const currentYear = new Date().getFullYear();
+    var operatingYear = "";
+    operatingYear += `<p class="operatingDate fs-6 m-0">Operating on ${currentYear}</div>`
+    document.getElementById("operatingTime").innerHTML = operatingYear;
+}
